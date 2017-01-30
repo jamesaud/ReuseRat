90eb6ec937b04d236a307237dbc42238414ba628
jamesaud <jamaudre@indiana.edu>
2017-01-30 09:33:26 -0500

    update model

--- reuserat/shipments/models.py
+++ reuserat/shipments/models.py
@@ -3,3 +3,16 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+
+
+class Shipment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # Shipment ID number
+    # Shipment status
+
+"""
+A shipment can have multiple items in it.
+"""
+class Item(models.Model):
+    #name = models.Char
+    # 
+    pass
