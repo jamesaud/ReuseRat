745eca1e7bc253eb4b12afd90a49b6ca2737dfc5
jamesaud <jamaudre@indiana.edu>
2017-02-01 15:15:03 -0500

    complete signup is required now before continuing to any other user view

--- reuserat/users/views.py
+++ reuserat/users/views.py
@@ -3,21 +3,61 @@ from __future__ import absolute_import, unicode_literals
 
 from django.core.urlresolvers import reverse
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 
 from .models import User
 
-from config.settings.common import GOOGLE_MAPS_API_KEY
 
-class UserDetailView(LoginRequiredMixin, DetailView):
+
+class UserCompleteSignupRequiredMixin(LoginRequiredMixin):
+    """
+    This should be inherited by Classes that should require a complete signup of user (payment info, etc.) before displaying
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Override the dispatch function, and required that a user have filled in payment type and address.
+        """
+        if not (self.request.user.payment_type and self.request.user.address):
+           return redirect('users:complete_signup', username=self.request.user.username)
+
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
+class UserCompleteSignupView(LoginRequiredMixin, UpdateView):
+
+    fields = ['name', 'payment_type', 'phone', 'address']
+
+    # we already imported User in the view code above, remember?
+    model = User
+
+    # send the user back to their own page after a successful update
+    def get_success_url(self):
+        return reverse('users:detail',
+                       kwargs={'username': self.request.user.username})
+
+    def get_object(self):
+        # Only get the User record for the user making the request
+        return User.objects.get(username=self.request.user.username)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserCompleteSignupView, self).get_context_data(**kwargs)
+        return context
+
+
+class UserDetailView(UserCompleteSignupRequiredMixin, DetailView):
     model = User
     # These next two lines tell the view to index lookups by username
     slug_field = 'username'
     slug_url_kwarg = 'username'
 
+    #def dispatch(self, request, *args, **kwargs):
+        #if not (self.request.user.payment_type and self.request.user.address):
+        #   return redirect('users:complete_signup')
+    #    return super(DetailView, self).dispatch(request, *args, **kwargs)
 
-class UserRedirectView(LoginRequiredMixin, RedirectView):
+class UserRedirectView(UserCompleteSignupRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
@@ -25,7 +65,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
                        kwargs={'username': self.request.user.username})
 
 
-class UserUpdateView(LoginRequiredMixin, UpdateView):
+class UserUpdateView(UserCompleteSignupRequiredMixin, UpdateView):
 
     fields = ['name', 'payment_type', 'phone', 'address']
 
@@ -44,6 +84,8 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super(UserUpdateView, self).get_context_data(**kwargs)
         return context
+
+
 
 class UserListView(LoginRequiredMixin, ListView):
     model = User
