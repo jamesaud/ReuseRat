e35c72125333445dbec8928519509b531684d845
jamesaud <jamaudre@indiana.edu>
2017-02-06 11:14:52 -0500

    made ui a bit better

--- reuserat/shipments/models.py
+++ reuserat/shipments/models.py
@@ -7,7 +7,7 @@ from reuserat.users.models import User
 class Shipment(models.Model):
     name =  models.CharField(max_length=200)
     user =  models.ForeignKey(User)
-    description =  models.CharField(max_length=1000,blank=True)
+    description =  models.CharField(max_length=1000, blank=False)
     created = models.DateTimeField(auto_now_add=True,)
     modified = models.DateTimeField(auto_now=True,)
 
