124682949f4a3ef93ed99586437efd07e94e5949
jamesaud <jamaudre@indiana.edu>
2017-02-09 02:20:35 -0500

    gulp compiling sass working, neeed to take out compressor

---
+++ reuserat/shipments/tests/factories.py
@@ -0,0 +1,16 @@
+import factory
+from reuserat.users.tests.factories import UserFactory
+
+
+class ShipmentFactory(factory.django.DjangoModelFactory):
+    name = factory.Sequence(lambda n: 'shipment-{0}'.format(n))
+    description = factory.Sequence(lambda n: 'shipment-{0}-description'.format(n))
+
+    user = factory.SubFactory(UserFactory)  # User is a foreign key, so we must use SubFactory
+
+    class Meta:
+        model = 'shipments.Shipment'
+        django_get_or_create = ('name', )
+
+
+
