5a74f243182990b214af9231e7f3d4bca3d906c6
jamesaud <jamaudre@indiana.edu>
2017-02-23 11:56:45 -0500

    knowledge base and production settings

--- reuserat/knowledge/urls.py
+++ reuserat/knowledge/urls.py
@@ -5,8 +5,14 @@ urlpatterns = [
         url(r'^shipping-guide/$', 
             TemplateView.as_view(template_name='knowledge/shipping.html'), 
             name='shipping'),
-
         url(r'^quickstart/$', 
             TemplateView.as_view(template_name='knowledge/quickstart.html'), 
             name='quickstart'),
+        url(r'^common-questions/$', 
+            TemplateView.as_view(template_name='knowledge/questions.html'), 
+            name='questions'),
+        url(r'^payment-information/$', 
+            TemplateView.as_view(template_name='knowledge/payment.html'), 
+            name='payment'),
+        
 ]
