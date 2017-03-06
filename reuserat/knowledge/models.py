d18ec3aefbaab40bf063b87a79c3a2f96be8d843
jamesaud <jamaudre@indiana.edu>
2017-03-06 18:46:34 -0500

    FAQ models created

---
+++ reuserat/knowledge/models.py
@@ -0,0 +1,30 @@
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+from reuserat.users.models import User
+
+class Shipment(models.Model):
+    user =  models.ForeignKey(User,
+                              on_delete=models.CASCADE)
+    name =  models.CharField(max_length=50)
+    description =  models.CharField(max_length=1000, blank=False)
+    is_physical = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def get_shipment_sku(self):
+        return "{0}-{1}".format(self.user_id, self.id)   # Set the sku to be <userid>-<shipmentid>
+
+    def get_visible_items(self):
+        return (item for item in self.item_set.filter(is_visible=True))
+
+    def has_visible_items(self):
+        return any(self.get_visible_items())
+
+    def __str__(self):
+        return "Name '{0}' for user '{1}'".format(self.name, str(self.user))
+
+    class Meta:
+        ordering = ('modified',)
