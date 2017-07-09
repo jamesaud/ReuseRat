22f0aa76ca7a2ea749bf716742976dc52c1713af
jamesaud <jamaudre@indiana.edu>
2017-07-09 15:14:20 -0400

    trying to get logging to work

--- reuserat/knowledge/views.py
+++ reuserat/knowledge/views.py
@@ -5,6 +5,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 def faq_view(request):
-    logger.error("FAQS page, this is a test", exec_info=True)
+    logger.error("FAQS page, this is a test", exc_info=True)
     faq_categories = [category for category in FAQCategory.objects.all() if category.faq_set.exists()]  # Filter out categories without FAQs
     return render(request, 'knowledge/questions.html', context={'faq_categories': faq_categories})
