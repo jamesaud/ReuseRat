12c033140f664059f3bc0096517dcdf77f2893fe
jamesaud <jamaudre@indiana.edu>
2017-07-09 15:42:44 -0400

    trying to get logging to work

--- reuserat/knowledge/views.py
+++ reuserat/knowledge/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, HttpResponse
 from reuserat.knowledge.models import FAQCategory
+from django.conf import settings
+
+
+
+from raven.handlers.logging import SentryHandler
+handler = SentryHandler(settings.SENTRY_DSN)
+handler.setLevel(logging.INFO)
+
+from raven.conf import setup_logging
+
+setup_logging(handler)
+
 
 import logging
 logger = logging.getLogger(__name__)
 
+
 def faq_view(request):
+
     logger.error("FAQS page, this is a test", exc_info=True)
+
     faq_categories = [category for category in FAQCategory.objects.all() if category.faq_set.exists()]  # Filter out categories without FAQs
     return render(request, 'knowledge/questions.html', context={'faq_categories': faq_categories})
