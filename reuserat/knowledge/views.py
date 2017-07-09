21de5749c7652f209d518e0c2e057d989a946e42
jamesaud <jamaudre@indiana.edu>
2017-07-09 17:37:44 -0400

    cicd

--- reuserat/knowledge/views.py
+++ reuserat/knowledge/views.py
@@ -15,12 +15,22 @@ def setup_logger():
 
         setup_logging(handler)
 
-setup_logger()
+
+if not settings.DEBUG:
+    # new logging code, because the above doesn't work
+    from raven.handlers.logging import SentryHandler
+    from raven.conf import setup_logging
+
+    handler = SentryHandler(settings.SENTRY_DSN)
+    handler.setLevel(logging.INFO)
+
+    setup_logging(handler)
+
 logger = logging.getLogger(__name__)
 
 def faq_view(request):
 
-    logger.error("FAQS page, this is another fantastic test", exc_info=True)
+    logger.error("FAQS page, this DSAFADSFASD another fantastic test", exc_info=True)
 
     faq_categories = [category for category in FAQCategory.objects.all() if category.faq_set.exists()]  # Filter out categories without FAQs
     return render(request, 'knowledge/questions.html', context={'faq_categories': faq_categories})
