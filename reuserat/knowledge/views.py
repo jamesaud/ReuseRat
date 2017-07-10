8dcf5584959b6a6ed11630833cd7da32f6bdcce0
jamesaud <jamaudre@indiana.edu>
2017-07-10 14:56:32 -0400

    logging essages;

--- reuserat/knowledge/views.py
+++ reuserat/knowledge/views.py
@@ -4,8 +4,9 @@ from reuserat.knowledge.models import FAQCategory
 import logging
 
 from config.logging import setup_logger
-
+logger = logging.getLogger(__name__)
 
 def faq_view(request):
+    logger.info("In Knowledge FAQs")
     faq_categories = [category for category in FAQCategory.objects.all() if category.faq_set.exists()]  # Filter out categories without FAQs
     return render(request, 'knowledge/questions.html', context={'faq_categories': faq_categories})
